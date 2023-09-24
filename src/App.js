49cc8ade6ae80461483d1dfb867b383add7a9f01
derangivishnu <derangivishnuvahan@yahoo.com>
2023-09-24 09:41:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ButtonAppBar from './components/navbar/Navbar';
+import RecipeReviewCard from './components/navbar/cards/Cards';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>apponix -test</h1>
+    <ButtonAppBar/>
+    <br/>
+    <RecipeReviewCard/>
+
+
+
+
+
     </div>
   );
 }
